ea81e24f2e35b59237e12bd54eedb41433de94ec
mosius27 <mosius27@yandex.ru>
2024-03-07 23:35:50 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './css/main.css';
+import ShopItemClass from './components/ShopItemClass';
+
+export default function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+    price: 399,
+    currency: '£',
+  };
+
+  return (
+    <div className="container">
+      <div className="background-element" />
+      <div className="highlight-window">
+        <div className="highlight-overlay" />
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />;
+      </div>
+    </div>
+  );
+}
